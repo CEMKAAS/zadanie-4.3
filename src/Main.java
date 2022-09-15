b740589ae8b3c3ceef3522822cf43a4e2503dcda
= <zaroslikob@mail.ru>
2022-09-15 22:29:32 +0300

    добавил сравнение, но еще пока не получилось

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,27 @@ import java.util.Map;
 
 public class Main {
 
-    public static String name = "lorel";
+    public static String name = "llloll";
 
     public static void main(String[] args) {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < name.length(); i++) {
             char aChar = name.charAt(i);
-            if(Character.isLetter(aChar)){
-                for (Character key : map.keySet()) {
-                    char a =;
-                    a.equals(map.get(key));
-
-                }
+            int x=0;
+            if(Character.isLetter(aChar) && map.put(aChar,1) == null){
+                x=1;
+                map.put(aChar,x);
+            }else {
+                x=map.get(aChar);
+                x++;
                 map.put(aChar,x);
             }
         }
-        System.out.println(map.values());
+               // map.put(aChar,x);
+        for (Map.Entry<Character, Integer> kv : map.entrySet()){
+            System.out.println(kv.getKey() + " " + kv.getValue());
+
+        }
+        }
+        //System.out.println(map.values());
     }
-}
