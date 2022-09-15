49ba37acf8f9753a7c0e17b378a81dddf2fb4d89
= <zaroslikob@mail.ru>
2022-09-15 23:15:08 +0300

    доделал увеличение значение буквы, если она повторяется, добавить максимум и минимум

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,20 @@ import java.util.Map;
 
 public class Main {
 
-    public static String name = "llloll";
+    public static String name = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     public static void main(String[] args) {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < name.length(); i++) {
             char aChar = name.charAt(i);
-            int x=0;
-            if(Character.isLetter(aChar) && map.put(aChar,1) == null){
-                x=1;
-                map.put(aChar,x);
-            }else {
-                x=map.get(aChar);
-                x++;
-                map.put(aChar,x);
+            if (Character.isLetter(aChar)) {
+                if (map.get(aChar) == null) {
+                    map.put(aChar, 1);
+                } else {
+                    int y = map.get(aChar);
+                    y++;
+                    map.put(aChar, y);
+                }
             }
         }
                // map.put(aChar,x);
